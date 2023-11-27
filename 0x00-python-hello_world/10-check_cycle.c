e3899bb424c63c3a3b303450b2133fe85e8d00df
Johnnharry88 <harryjohnwhye@gmail.com>
2023-11-27 12:51:17 +0100

    Python assignment

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * check_cycle - function that checks for a reapetition or cycle
+ * in a singly linked list
+ * @lx: pointer to head of singly linked lists
+ * Return: 0 if no cycle otherwise, 1.
+ */
+int check_cycle(listint_t *lx)
+{
+	listint_t *check_1 = NULL, *check_2 = NULL;
+
+	if (lx == NULL)
+		return (0);
+	check_1 = lx;
+	check_2 = lx->next;
+	while (check_1 && check_2->next)
+	{
+		check_1 = check_1->next;
+		check_2 = check_2->next->next;
+
+		if (check_2 == check_1)
+			return (1);
+	}
+	return (0);
+}
---
+++ 0x00-python-hello_world/README.md
@@ -0,0 +1,1 @@
+First Python programming project
