d3652b02ef5303147d84af15d8a45571a9f5f7e8
Johnnharry88 <harryjohnwhye@gmail.com>
2023-11-30 16:07:21 +0100

    Project Python

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import sys
-ans = 0
-sch = len(sys.argv)
-for alx in range(1, sch):
-    ans = ans + int(sys.argv[alx])
-print("{}".format(ans))
+if __name__ == "__main__":
+    import sys
+    ans = 0
+    sch = len(sys.argv)
+    for alx in range(1, sch):
+        ans = ans + int(sys.argv[alx])
+    print("{}".format(ans))
