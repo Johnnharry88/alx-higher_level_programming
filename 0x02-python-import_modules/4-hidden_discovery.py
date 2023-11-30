30d784788a5c87951df81a09e61accaa1198f908
Johnnharry88 <harryjohnwhye@gmail.com>
2023-11-30 16:10:17 +0100

    Project Python

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-form hidden_4 import *
-store = dir()
-x = len(store)
-for alx in range (0, x):
-    if store[alx][:2] != "__":
-        print("{}".format(store[alx]))
+if __name__ == "__main__":
+    form hidden_4 import *
+    store = dir()
+    x = len(store)
+    for alx in range(0, x):
+        if store[alx][:2] != "__":
+            print("{}".format(store[alx]))
