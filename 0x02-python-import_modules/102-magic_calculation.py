ad9e49efd0e2db44a358f869357d6d06607706b8
Johnnharry88 <harryjohnwhye@gmail.com>
2023-11-30 16:55:44 +0100

    Project Python

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,10 +2,10 @@
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
-        alx = add(a, b)
-        for sch in range(4, 6):
-            g = add(alx, sch)
-        return g
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
     else:
         c = sub(a, b)
         return c
