1af4fde38455f449af8502259607a344a3474ff4
Johnnharry88 <harryjohnwhye@gmail.com>
2023-11-30 16:53:05 +0100

    Project Python

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,8 +3,7 @@ if __name__ == "__main__":
     form hidden_4 import hidden_4
 
     store = dir(hidden_4)
-    x = len(store)
 
-    for alx in range(0, x):
+    for alx in store:
         if alx[:2] != "__":
             print(alx)
