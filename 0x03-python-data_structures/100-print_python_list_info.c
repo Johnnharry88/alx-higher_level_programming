382456743ad901dfe6de37a2e6aaa2304ed13d59
Johnnharry88 <harryjohnwhye@gmail.com>
2023-12-02 21:06:53 +0100

    Project Python

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -6,19 +6,19 @@
  */
 void print_python_list_info(PyObject *p)
 {
-	int x, y , z;
+	int x, y, z;
 	PyObject *v;
 
 	x = Py_SIZE(p);
-	y = ((PyListObject *)p)-> allocated;
+	y = ((PyListObject *)p)->allocated;
 
 	printf("[*] Size of the Python List = 5d\n", x);
 	printf("[*] Allocated = %d\n", y);
 
-	for (z = 0, z < x; z++)
+	for (z = 0; z < x; z++)
 	{
 		printf("Element %d: ", z);
 		v = PyList_GetItem(p, z);
-		printf("%s\n",Py_TYPE(v)->typ_name);
+		printf("%s\n", Py_TYPE(v)->typ_name);
 	}
 }
