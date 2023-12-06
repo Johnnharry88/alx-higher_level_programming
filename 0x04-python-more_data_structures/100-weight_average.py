89d62e223b465dc1972e9eed3b5c56c3d2b4ce4d
Johnnharry88 <harryjohnwhye@gmail.com>
2023-12-06 13:55:31 +0100

    Project Python

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    x = len(my_list)
+    if not isinstance(my_list, list) or x == 0:
+        return 0
+    else:
+        alx = 0
+        xty = 0
+        for x in my_list:
+            alx = alx + (x[0] * x[1])
+            xty = xty + (x[1])
+            ans = alx / xty
+            return ans
