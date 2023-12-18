a4660aafd0eb1d85627f75df665af27fc4a8bcce
Johnnharry88 <harryjohnwhye@gmail.com>
2023-12-18 13:55:27 +0100

    Python Exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    t_sum = 0
+    for a in range(x):
+        try:
+            print(f"{my_list[a]}", end="")
+            t_sum = t_sum + 1
+        except IndexError:
+            break
+    print()
+    return(t_sum)
