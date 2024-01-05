4342934100206da02de326647425a00ddab09da5
Johnnharry88 <harryjohnwhye@gmail.com>
2024-01-05 08:57:23 +0100

    Python class

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""A Class Square"""
+
 
 class Square:
+    """Body of class square"""
+
     def __init__(self, size=0):
+        """Initializing class square
+        Size: size of class of integer type
+        Exception Error:
+            TypeError: if size is not integer
+            ValueError: if is less tham zero
+        """
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
@@ -11,6 +21,8 @@ class Square:
 
     @property
     def size(self):
+        """Gess that size of square"""
+
         return self.__size
 
     @size.setter
@@ -22,9 +34,16 @@ class Square:
         self.__size = value
 
     def area(self):
+        """
+        Estimates tha area of square
+        Return: Square raised to power of 2
+        """
+
         return (self.__size ** 2)
 
     def my_print(self):
+        """prints out the square in #"""
+
         if self.__size == 0:
             print()
         for x in range(self.__size):
