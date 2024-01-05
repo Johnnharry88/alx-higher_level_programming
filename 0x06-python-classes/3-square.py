b786fdf9d08014147ad0bac5f00abd570fb98cc7
Johnnharry88 <harryjohnwhye@gmail.com>
2024-01-05 08:26:43 +0100

    Python Class

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""Defines a  class Square"""
+
 
 class Square:
+    """Body of class Square"""
+
     def __init__(self, size=0):
+        """Initializing the class Square
+        Size: size of class square
+        Error Exception:
+            TypeError: if size is not integer
+            ValueEror: if size is less than zero
+        """
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
