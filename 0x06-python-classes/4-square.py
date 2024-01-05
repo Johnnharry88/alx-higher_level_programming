0ebd3978982d4125f52e8520b809d0978b35bd9f
Johnnharry88 <harryjohnwhye@gmail.com>
2024-01-05 08:39:40 +0100

    Ptython class

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
+"""Defines a Clas Square"""
+
 
 class Square:
+    """Body of Class Square"""
+
     def __init__(self, size=0):
+        """Initializing the class square
+        Size: size of the square defined
+        Exception Error:
+        TypeError: if size is not an integer
+        ValueError: if size is less than zero
+        """
+
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
@@ -10,6 +21,8 @@ class Square:
 
     @property
     def size(self):
+        """Gets the size of square"""
+
         return self.__size
 
     @size.setter
@@ -21,4 +34,9 @@ class Square:
         self.__size = value
 
     def area(self):
+        """
+        Estimates the area of the square
+        Return: square of size
+        """
+
         return (self.__size ** 2)
