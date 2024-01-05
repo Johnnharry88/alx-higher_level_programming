b935e0759a7b77367ad087d1d241bff368354350
Johnnharry88 <harryjohnwhye@gmail.com>
2024-01-05 07:59:59 +0100

    Pythn class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+
+"""Defines Class Square"""
+
 class Square:
+    """Body of Class"""
+
     def __init__(self, size):
+        """Initializing the Square class
+        Size - the size of square
+        """
         self.__size = size
