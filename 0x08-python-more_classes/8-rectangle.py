4875f0a68fca7f3e3e112611b08dfd3ef23b29fe
Johnnharry88 <harryjohnwhye@gmail.com>
2024-01-06 07:18:27 +0100

    Pyhton Object

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -88,8 +88,10 @@ class Rectangle:
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 musst be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() > rect_2.area():
+            return rect_1
+        elif rect_1.area() == rect_2.area():
             return rect_1
         else:
             return rect_2
