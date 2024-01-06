b2ab3c704134ad984a05348c77ef5c1028acb24c
Johnnharry88 <harryjohnwhye@gmail.com>
2024-01-06 04:56:29 +0100

    Pythn Object

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -51,11 +51,11 @@ class Rectangle:
 
     def perimeter(self):
         """Returns the perimenter of rectangle"""
-        if  self.__width == 0 or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return (0)
         else:
-            return ((self.__width *2) + (self.__height * 2))
-    
+            return ((self.__width * 2) + (self.__height * 2))
+
     def __str__(self) -> str:
         """outputs diagram of rectangle defined for an obkject"""
         if self.__width == 0 or self.__height == 0:
