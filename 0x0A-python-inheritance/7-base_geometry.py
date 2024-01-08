eabb34456f00c867f9880fea73dcabcb1692654d
Johnnharry88 <harryjohnwhye@gmail.com>
2024-01-08 15:50:22 +0100

    Python Inheritance

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a baseGeometry Class"""
+
+
+class BaseGeometry:
+    """ Defined the bdy of class Base Geometry"""
+    def area(self):
+        """IMehtod not implemenented yet"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, anme, value):
+        """Validates if value is an integer"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value < = 0:
+            raise ValueError("{} must be greater than 0".format(name))
