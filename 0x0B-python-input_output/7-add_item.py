8ec027420c227e46eb0a72f09ad3fc554dec8b8b
Johnnharry88 <harryjohnwhye@gmail.com>
2024-01-09 20:48:11 +0100

    Python Inputt//Output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,14 +6,12 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = 'add_items.json'
-my_list = []
-try:
-    my_list = load_from_json_file(filename)
-except Exception:
-    save_to_json_file(my_list, filename)
 
-arglen = len(argv)
-if arglen > 1:
-    for x in range(1, arglen):
-        my_list.append(argv[x])
+try:
+    json_list = load_from_json(filename)
+except:
+    json_list =[]
+    for x inargv[1:]:
+        json_list.append(x)
+
     save_to_json_file(my_list, filename)
