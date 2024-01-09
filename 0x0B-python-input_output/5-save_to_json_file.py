264db8663806ec8f5b5e717c9efbad19f43acdcd
Johnnharry88 <harryjohnwhye@gmail.com>
2024-01-09 15:20:49 +0100

    Python Input/Output

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pythin3
+"""Module contains JAON file-writing function"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Retunrs a text file written suing Json model"""
+    with open(filename, "w") as x:
+        a = json.dump(my_obj, x)
+        return a
