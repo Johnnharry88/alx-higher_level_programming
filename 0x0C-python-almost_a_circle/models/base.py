875e1b785ba77257e8d300caf27c8254bb21390d
Johnnharry88 <harryjohnwhye@gmail.com>
2024-01-16 03:58:58 +0100

    Python circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,6 +25,9 @@ class Base:
         """return the string representation of list dictionaries"""
         if list_dictionaries == [] or list_dictionaries is None:
             return "[]"
+        if (type(list_dictionaries) != list or not
+                all(type(x) == dict for x in list_dictionaries)):
+            raise TypeError("list_dictionaries must be a list of dictionaries")
         return json.dumps(list_dictionaries)
 
     @staticmethod
@@ -48,95 +51,95 @@ class Base:
                 list_dict = [x.to_dictionary() for x in list_objs]
                 j.write(Base.to_json_string(list_dict))
 
-        @classmethod
-        def load_from_file(cls):
-            """loads json file """
-            xty = cls.__name__ + ".json"
-            list_of_inst = []
-            list_of_dict = []
-            if os.path.exists(xty):
-                with open(xty, "r") as p:
-                    str_rd = xty.read()
-                    list_dict = cls.from_json_str(str_rd)
-                    for d in list_dict:
-                        list_of_inst.append(cls.create(**dictionary))
-            return list_of_inst
+    @classmethod
+    def load_from_file(cls):
+        """loads json file """
+        xty = cls.__name__ + ".json"
+        list_of_inst = []
+        list_of_dict = []
+        if os.path.exists(xty):
+            with open(xty, "r") as p:
+                str_rd = xty.read()
+                list_dictionaries = cls.from_json_string(str_rd)
+                for d in list_dictionaries:
+                    list_of_inst.append(cls.create(**dictionary))
+        return list_of_inst
 
-        @classmethod
-        def create(cls, **dictionary):
-            """Returns instance with attributes set"""
-            if cls.__name__ == 'Rectangle':
-                alx = cls(1, 1)
-            elif cls.__name__ == 'Square':
-                alx = cls(1)
-            alx.update(**dictionary)
-            return alx
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns instance with attributes set"""
+        if cls.__name__ == 'Rectangle':
+            alx = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            alx = cls(1)
+        alx.update(**dictionary)
+        return alx
 
-        @classmethod
-        def save_to_file_csv(cls, list_objs):
-            """Serializes list_objs and saves to file"""
-            xty = cls.__name__ + ".csv"
-            with open(xty, "w", newline="") as file_csv:
-                if list_objs is None or list_objs == []:
-                    file_csv.write("[]")
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Serializes list_objs and saves to file"""
+        xty = cls.__name__ + ".csv"
+        with open(xty, "w", newline="") as file_csv:
+            if list_objs is None or list_objs == []:
+                file_csv.write("[]")
+            else:
+                if cls.__name__ == "Rectangle":
+                    parameters = ["id", "width", "height", "x", "y"]
                 else:
-                    if cls.__name__ == "Rectangle":
-                        parameters = ["id", "width", "height", "x", "y"]
-                    else:
-                        parameters = ["id", "size", "x", "y"]
-                    writter = csv.DictWriter(file_csv, fieldnames=parameters)
-                    for x in list_objs:
-                        writter.writerow(obj.to_dictionary())
+                    parameters = ["id", "size", "x", "y"]
+                writter = csv.DictWriter(file_csv, fieldnames=parameters)
+                for x in list_objs:
+                    writter.writerow(obj.to_dictionary())
 
-        def load_from_file_csv(cls):
-            """Deserializes Csv fomaat in a file"""
-            xty = cls.__name__ + ".csv"
-            try:
-                with open(xty, "r", newline="") as file_csv:
-                    if cls.__name__ == "Rectangle":
-                        parameters = ["id", "width", "height", "x", "y"]
-                    else:
-                        parameters = ["id", "size", "x", "y"]
-                    list_objs = csv.DictReader(file_csv, fieldnames=parameters)
-                    list_objs = [dict([x, int(y)] for x, y in d.items())
-                                 for d in list_objs]
-                    return [cls.ceate(**d) for d in list_objs]
-            except IOError:
-                return []
+    def load_from_file_csv(cls):
+        """Deserializes Csv fomaat in a file"""
+        xty = cls.__name__ + ".csv"
+        try:
+            with open(xty, "r", newline="") as file_csv:
+                if cls.__name__ == "Rectangle":
+                    parameters = ["id", "width", "height", "x", "y"]
+                else:
+                    parameters = ["id", "size", "x", "y"]
+                list_objs = csv.DictReader(file_csv, fieldnames=parameters)
+                list_objs = [dict([x, int(y)] for x, y in d.items())
+                             for d in list_objs]
+                return [cls.ceate(**d) for d in list_objs]
+        except IOError:
+            return []
 
-        @staticmethod
-        def draw(list_rectangles, list_squares):
-            """Dwaw Rectangles and squares usiing the turtle module.
-            Arguments:
-                list_rectangles (list): List of retangle object to be drawn.
-                list_squares (list): A list of square objects to be drawn
-            """
-            drw = turtle.Turtle()
-            drw.screen.bgcolor("#c7333b")
-            drw.pensize(9)
-            drw.shape("turtle")
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """Dwaw Rectangles and squares usiing the turtle module.
+        Arguments:
+            list_rectangles (list): List of retangle object to be drawn.
+            list_squares (list): A list of square objects to be drawn
+        """
+        drw = turtle.Turtle()
+        drw.screen.bgcolor("#c7333b")
+        drw.pensize(9)
+        drw.shape("turtle")
 
-            drw.color("#ffffff")
-            for r in list_rectangles:
-                drw.showturtle()
-                drw.up()
-                drw.got(rect.x, rect.y)
-                for i in range(2):
-                    drw.forward(rect.width)
-                    drw.left(90)
-                    drw.forward(rect.height)
-                    drw.left(90)
-                drw.hideturtle()
+        drw.color("#ffffff")
+        for r in list_rectangles:
+            drw.showturtle()
+            drw.up()
+            drw.got(rect.x, rect.y)
+            for i in range(2):
+                drw.forward(rect.width)
+                drw.left(90)
+                drw.forward(rect.height)
+                drw.left(90)
+            drw.hideturtle()
 
-            drw.color("#c5e3d8")
-            for s in list_squares:
-                drw.showturtle()
-                drw.up()
-                drw.goto(sq.x, sq.y)
-                drw.down()
-                for i in range(2):
-                    drw.forward(sq.width)
-                    drw.left(90)
-                    drw.forward(sq.height)
-                    drw.left(90)
-                drw.hideturtle()
+        drw.color("#c5e3d8")
+        for s in list_squares:
+            drw.showturtle()
+            drw.up()
+            drw.goto(sq.x, sq.y)
+            drw.down()
+            for i in range(2):
+                drw.forward(sq.width)
+                drw.left(90)
+                drw.forward(sq.height)
+                drw.left(90)
+            drw.hideturtle()
