1be7a6cf2bee6c02cc97038377274ea693908ee7
Johnnharry88 <harryjohnwhye@gmail.com>
2024-02-14 21:35:17 +0100

    MySQL

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- creates the database hbtm_Od_usa and table states in MySQL server
 
-create database if not eixsts hbtn_0d_usa;
+create database if not exists hbtn_0d_usa;
 create table if not exists hbtn_0d_usa.states (id int not null primary key auto_increment, name varchar(256) not null);
