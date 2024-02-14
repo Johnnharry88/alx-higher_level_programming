f8bf418decbf020f71e2e826503acb317b87a409
Johnnharry88 <harryjohnwhye@gmail.com>
2024-02-14 21:59:36 +0100

    Projet

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- lists all the cities of California that si in database 
 
-select id, name from cities where state = (select id from states where name = 'California') order by id;
+select id, name from cities where state_id = (select id from states where name = 'California') order by id;
