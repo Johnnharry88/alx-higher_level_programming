4a268240e15451b52eb5fe06b62bed59be81a21c
Johnnharry88 <harryjohnwhye@gmail.com>
2024-02-14 21:21:50 +0100

    MySQL

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 
 
 create database if not exists 'hbtn_0d_2';
-create user if not exists 'user_0d_2'@'localost' identified by 'user_0d_2_pwd';
+create user if not exists 'user_0d_2'@'localhost' identified by 'user_0d_2_pwd';
 grant select on 'hbtn_0d_2'.* to 'user_0d_2'@'localhost';
 flush privileges;
