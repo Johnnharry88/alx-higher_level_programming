c8332912e5c45333f74d97d2d8393b4be5b0ff34
Johnnharry88 <harryjohnwhye@gmail.com>
2024-02-15 02:31:03 +0100

    Project

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,7 @@
 --Lists all cities found in database 
 
-select cities.id, cities.name, states.name
-from cities
-inner join states on cities.state_id=states.id
-order by cities.id;
+select c.'id', c.'name', s.'name'
+from 'cities' as c 
+inner join 'states' as s
+on c.'state_id'= s.'id'
+order by c.'id';
