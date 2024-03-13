ef46568500c9540798f6fd41f614ae5431b64f4f
Johnnharry88 <harryjohnwhye@gmail.com>
2024-03-13 11:50:35 +0100

    Java

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-module.exports = class Square extends require ('./5-square') {
+module.exports = class Square extends require('./5-square') {
   charPrint (c) {
-    if (c == undefined) {
-      this.print ();
+    if (c === undefined) {
+      this.print();
     } else {
       for (let y = 0; y < this.height; y++) console.log(c.repeat(this.width));
     }
