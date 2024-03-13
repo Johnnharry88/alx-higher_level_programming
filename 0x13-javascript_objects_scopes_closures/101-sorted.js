c11aab5895dc64df202903c43b115e707e116a60
Johnnharry88 <harryjohnwhye@gmail.com>
2024-03-13 15:03:21 +0100

    Java

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-let n_dict = {};
-for (let k in dict) {
-  if (n_dict[dict[k]] === undefined) {
-    n_dict[dict[k]] = [k];
+const ndict = {};
+for (const k in dict) {
+  if (ndict[dict[k]] === undefined) {
+    ndict[dict[k]] = [k];
   } else {
-    n_dict[dict[k]].push(k);
+    ndict[dict[k]].push(k);
   }
 }
-console.log(n_dict);
+console.log(ndict);
