7af15ef0e29e1939fd14756014f2c8d7599157f2
Johnnharry88 <harryjohnwhye@gmail.com>
2024-03-13 04:56:48 +0100

    Java

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+module.exports = class Square extends require ('./5-square') {
+  charPrint (c) {
+    if (c == undefined) {
+      this.print ();
+    } else {
+      for (let y = 0; y < this.height; y++) console.log('C'.repeat(this.width));
+    }
+  }
+};
