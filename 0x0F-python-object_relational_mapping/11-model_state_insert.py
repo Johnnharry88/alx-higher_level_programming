e5d4890dfae9265719c1eb1a5cd8f0952457517b
Johnnharry88 <harryjohnwhye@gmail.com>
2024-03-18 23:47:21 +0100

    Python SQl

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-#lists all state object in the database
+"""Inserts argument in table state in the database"""
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sesionmaker
-from model_state import State
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
-    emgine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                            .format(sysmargv[1], sys,argv[2], sys.argv[3]),
-                            pool_pre_ping=True)
-    Session = sessionmakeer(bind=engine)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
 
     add_louis = State(name="Louisiana")
