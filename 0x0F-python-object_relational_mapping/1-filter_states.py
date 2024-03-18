7631185a6f052836cf31f351662aa56f974c3a55
Johnnharry88 <harryjohnwhye@gmail.com>
2024-03-18 20:02:56 +0100

    Python SQL

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-# list all states with name starting with N form database
+"""Scripts that list all states having a in it"""
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    x = db.cursor()
-    x.execute("SELCET * FROM 'states' ORDER BY 'id'")
-    [print(a) for a in c.fetchall() if a[1][0] = "N"] 
+    c = db.cursor()
+    c.execute("SELECT * FROM states ORDER BY id")
+    [print(a) for a in c.fetchall() if a[1][0] == 'N'] 
