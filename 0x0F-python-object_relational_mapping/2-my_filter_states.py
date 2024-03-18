bec230d8587b2d48aa247a6848457ad9c8626074
Johnnharry88 <harryjohnwhye@gmail.com>
2024-03-18 20:17:19 +0100

    Python SQL

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-# Displays all states with vlaue matchin the supplied argument
+"""Displays all values in states held in database hbtm_0e_0_usa"""
 import sys
 import MySQLdb
 
-if __name == '__main__':
+if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     a = db.cursor()
     a.execute("SELECT * \
-                FROM 'states' \
-               WHERE BINARY 'name' = '{}'".format(sys,argv[4]))
+                FROM states \
+               WHERE BINARY name = '{}'".format(sys.argv[4]))
     [print(x) for x in a.fetchall()]
