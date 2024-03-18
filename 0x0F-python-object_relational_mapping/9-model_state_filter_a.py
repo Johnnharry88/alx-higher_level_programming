4f1d62e1a7a832d0c765acc173ce2d8db8cbf77e
Johnnharry88 <harryjohnwhye@gmail.com>
2024-03-18 23:21:35 +0100

    Python SQl

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -2,16 +2,17 @@
 #lists all state object in the database
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sesionmaker
+from sqlalchemy.orm import sessionmaker
 from model_state import State
 
 if __name__ == "__main__":
-    emgine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                            .format(sysmargv[1], sys,argv[2], sys.argv[3]),
-                            pool_pre_ping=True)
-    Session = sessionmakeer(bind=engine)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
     session = Session()
 
     for state in session.query(State).order_by(State.id):
         if 'a' in state.name:
             print("{}: {}".format(state.id, state.name))
+    session.close()
