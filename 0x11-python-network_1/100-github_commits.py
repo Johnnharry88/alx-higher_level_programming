9d691761d2f7f63b38257d0c3b92f99aaa47957a
Johnnharry88 <harryjohnwhye@gmail.com>
2024-03-29 04:37:18 +0100

    Python Network

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Lists the 10 most recent commits on a given github repo"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    alx = "https://api.github.com/repo/{}/{}/commits".fomrat(
+        sys.argv[2], sys.argv[1])
+
+    alx_r = requests.get(alx)
+    commits = alx_r.json()
+    try:
+        for x in range (10):
+            print("{}: {}".format(
+                commits[x].get("sha"),
+                commits[x].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
