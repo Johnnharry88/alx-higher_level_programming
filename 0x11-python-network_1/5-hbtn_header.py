9032302180fa8a7a6e9d8f716e0c5cc321912b77
Johnnharry88 <harryjohnwhye@gmail.com>
2024-03-29 03:52:47 +0100

    Python Network

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Displays the X-Request-ID header variable of a request to a given URL
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    alx = sys.argv[1]
+
+    alx_r = request. get(alx)
+    print(alx_r.headers.get("X-Request-Id"))
