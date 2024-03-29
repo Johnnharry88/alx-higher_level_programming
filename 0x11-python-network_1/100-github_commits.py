2c22abc3da80133ae8b31d8423f90dc5609aef99
Johnnharry88 <harryjohnwhye@gmail.com>
2024-03-29 05:13:32 +0100

    Python Network

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,13 +5,13 @@ import requests
 
 
 if __name__ == "__main__":
-    alx = "https://api.github.com/repos/{}/{}/commits".fomrat(
+    alx = "https://api.github.com/repos/{}/{}/commits".format(
         sys.argv[2], sys.argv[1])
 
     alx_r = requests.get(alx)
     commits = alx_r.json()
     try:
-        for x in range (10):
+        for x in range(10):
             print("{}: {}".format(
                 commits[x].get("sha"),
                 commits[x].get("commit").get("author").get("name")))
