e12eca53e4545d365d17c46acdf75a993cb11ded
Johnnharry88 <harryjohnwhye@gmail.com>
2024-03-29 04:27:02 +0100

    Python Network

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Send the github API ot display a github ID baed on given credentials
+"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    key = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    alx_r = requests.get("https://api.github.come/user", auth=key)
+    print(alx_r.json().get("id"))
