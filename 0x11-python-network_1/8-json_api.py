eb7be413c150df189ecbc524a891c57d62cad55f
Johnnharry88 <harryjohnwhye@gmail.com>
2024-03-29 04:19:00 +0100

    Python Network

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    search = "" if len(sys.argv) ==  1 else sys.argv[1]
+    load = {"q": search}
+
+    alx_r = requests.post("http://0.0.0.0:5000/search_user", data=load)
+    try:
+        res = alx_r.json()
+        if res == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(res.get("id"), res.get("name"0)))
+    except ValueError:
+        print("Not a vlaid JSON")
