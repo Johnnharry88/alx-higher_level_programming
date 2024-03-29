b19eda966cf9d8a3f39ae487061b8ea9536eee40
Johnnharry88 <harryjohnwhye@gmail.com>
2024-03-29 03:40:40 +0100

    Python Network

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.bbtn.io/status."""
+import requests
+
+
+if __name__ == "__main__":
+    alx = request.get("https://alx-intranet.hbtn.io/status")
+    print("Body response")
+    print("\t- type: {}".format(type(alx.test)))
+    print("\t- content: {}".format(alx.test))
