1ddcc2451525a2370484d4813c95069d2722a31e
Johnnharry88 <harryjohnwhye@gmail.com>
2024-04-25 21:45:58 +0100

    Javascript

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+const urlx = process.argv[2];
+
+request.get(urlx, { json: true }, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const completed = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!completed[todo.userId]) {
+        completed[todo.userId] = 1;
+      } else {
+        completed[todo.userId] += 1;
+      }
+    }
+  });
+  console.log(completed);
+})
