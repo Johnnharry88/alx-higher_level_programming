a38e383349555763efeefbd03968fe0134b0361a
Johnnharry88 <harryjohnwhye@gmail.com>
2024-04-25 20:53:08 +0100

    Javascript

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const file_name = process.argv[2];
+const filename = process.argv[2];
 const cont = process.argv[3];
 
-fs.writeFile(file_name, cont, 'utf-8', (error) => {
+fs.writeFile(filename, cont, 'utf-8', (error) => {
   if (error) {
     console.log(error);
   }
