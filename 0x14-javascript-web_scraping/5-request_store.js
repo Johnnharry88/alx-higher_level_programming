aecfa3916e028b72cef29002658ffbef4f4dd3e7
Johnnharry88 <harryjohnwhye@gmail.com>
2024-04-25 21:34:31 +0100

    Javascript

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,14 +2,14 @@
 
 const request = require('request');
 const fs = require('fs');
-const url_x = process.argv[2];
-const file_x = process.argv[3];
+const urlx = process.argv[2];
+const filex = process.argv[3];
 
-request(url_x, (error, response, body) => {
+request(urlx, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    fs.writeFile(file_x, body, 'utf-8', (error) => {
+    fs.writeFile(filex, body, 'utf-8', (error) => {
       if (error) {
         console.log(error);
       }
